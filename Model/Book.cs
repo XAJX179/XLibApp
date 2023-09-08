67a906be9c6b8a82e34b8e2221650caa8bd2e949
XAJX <xajx179@gmail.com>
2023-09-08 19:43:20 +0530

    Add project files.

---
+++ Model/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XLibApp.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+    }
+}
---
+++ Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using XLibApp.Model;
+
+namespace XLibApp.Pages.BookList
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var BookfromDb = await _db.Book.FindAsync(Book.Id);
+                BookfromDb.Title = Book.Title;
+                BookfromDb.Author = Book.Author;
+                BookfromDb.Type = Book.Type;
+                BookfromDb.Quantity = Book.Quantity;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+    }
+}
